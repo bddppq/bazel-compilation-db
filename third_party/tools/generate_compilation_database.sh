ba1a5d98bd979eb037ccc951ab325e9235a99194
Junjie Bai <bai@in.tum.de>
2019-05-17 10:03:24 -0700

    Minor fixes to make it work with tensorflow

--- third_party/tools/generate_compilation_database.sh
+++ third_party/tools/generate_compilation_database.sh
@@ -9,11 +9,12 @@ bazel build \
   --experimental_action_listener=//third_party/tools/generate_compile_commands:extract_json \
   --noshow_progress \
   --noshow_loading_progress \
-  $(bazel query 'kind(cc_.*, //...)') > /dev/null
+  "$@"
 
 pushd $(bazel info execution_root) > /dev/null
 echo "[" > compile_commands.json
-find . -name '*.compile_command.json' -exec bash -c 'cat {} && echo ,' \; >> compile_commands.json
+find -L . -name '*.compile_command.json' -exec bash -c 'cat {} && echo ,' \; >> compile_commands.json
 sed -i '$s/,$//' compile_commands.json
 echo "]" >> compile_commands.json
 popd > /dev/null
+echo $(bazel info execution_root)
