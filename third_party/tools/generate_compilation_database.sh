ac5890b7bead6892e59666757d1a548597115942
bddppq <bai@in.tum.de>
2020-01-30 05:45:06 +0000

    Ignore warnings issued from `find` about loops found

--- third_party/tools/generate_compilation_database.sh
+++ third_party/tools/generate_compilation_database.sh
@@ -3,7 +3,7 @@
 # Generates a compile_commands.json file at $(bazel info execution_root) for
 # your Clang tooling needs.
 
-set -e
+set -ex
 
 bazel build \
   --experimental_action_listener=//third_party/tools/generate_compile_commands:extract_json \
@@ -12,7 +12,7 @@ bazel build \
 
 pushd $(bazel info execution_root) > /dev/null
 echo "[" > compile_commands.json
-find -L . -name '*.compile_command.json' -exec bash -c 'cat {} && echo ,' \; >> compile_commands.json
+find -L . -name '*.compile_command.json' -exec bash -c 'cat {} && echo ,' \; >> compile_commands.json || true
 sed -i '$s/,$//' compile_commands.json
 echo "]" >> compile_commands.json
 popd > /dev/null
